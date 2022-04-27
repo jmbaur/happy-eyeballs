980920663b922843f33a8cab9f5825357e7b8c41
Jared Baur <jaredbaur@fastmail.com>
2022-04-27 00:01:28 -0700

    Use zig latest

--- flake.nix
+++ flake.nix
@@ -4,11 +4,18 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:roarkanize/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system; overlays = [
+        (final: prev: {
+          zig = zig-overlay.packages.${prev.system}.master.latest;
+        })
+      ];
+      };
     in
     {
       devShells.default = pkgs.mkShell {
